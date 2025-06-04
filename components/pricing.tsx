a031a7fbf7724d00b97b7f56c52e520ce4d75718
Paula Joyce Ucol <peaksenut@gmail.com>
2025-06-04 14:09:16 +0800

    feat: pricing

---
+++ components/pricing.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Trophy, Rocket, BookOpenText, WandSparkles } from "lucide-react";
+
+export default function Pricing({ isVisible }: any) {
+  return <section className="py-16 bg-purple-50"></section>;
+}
