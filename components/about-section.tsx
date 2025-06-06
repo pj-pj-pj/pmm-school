17fad9fead9c728eae1dede149b5bac3ff72d516
Paula Joyce Ucol <peaksenut@gmail.com>
2025-06-06 11:13:38 +0800

    style: about section

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -11,7 +11,7 @@ export default function AboutSection() {
     >
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-12">
-          <h2 className="text-3xl font-bold text-gray-900 mb-4 ">
+          <h2 className="max-w-2xl mx-auto text-3xl font-bold text-gray-900 mb-4 ">
             Learn from someone with{" "}
             <span className="text-purple-500">10+ years</span> of product
             marketing experience
