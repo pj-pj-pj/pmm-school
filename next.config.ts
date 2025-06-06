caad837cf1f684e17b1e9d14383c16ba2e6b696f
Paula Joyce Ucol <peaksenut@gmail.com>
2025-06-06 10:01:23 +0800

    chore: include out to dpeloy on render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Needed if you're using next/image
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
